1edec196f3f9e629164069e9b63367309c348255
juanmarchant <jua.marchant@duocuc.cl>
2024-05-01 11:32:42 -0400

    counter useSelector

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+import { useDispatch, useSelector } from 'react-redux'
+import { increment, decrement, incrementBy } from './store/slices/counter'
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { counter } = useSelector((state) => state.counter)
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -17,17 +22,25 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>count is {counter}</p>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch(increment())}>
+          increment
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="card">
+        <button onClick={() => dispatch(decrement())}>
+          decrement
+        </button>
+      </div>
+
+      <div className="card">
+        <button onClick={() => dispatch(incrementBy(5))}>
+          increment
+        </button>
+      </div>
     </>
   )
 }
