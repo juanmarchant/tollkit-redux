8a34146a8ac7d14a613122081cb51ace773c1dfb
juanmarchant <jua.marchant@duocuc.cl>
2024-05-01 13:56:08 -0400

    pokemon with redux and toolkit {axios- thunks}

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-
-import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store'
 
+
+import App from './App.jsx'
+import { PokemonApp } from './PokemonApp.jsx'
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </Provider>
   </React.StrictMode>,
 )
