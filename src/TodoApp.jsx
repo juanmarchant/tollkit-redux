dad7f90cf0ced648f2584e9d8882cf25626995d3
juanmarchant <jua.marchant@duocuc.cl>
2024-05-01 18:23:58 -0400

    change fake data  {prev btn - next btn}

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,28 +1,46 @@
-import { useGetTodosQuery } from "./store/apis"
-
+import { useState } from "react";
+import { useGetTodoQuery, useGetTodosQuery } from "./store/apis"
 
 
 export const TodoApp = () => {
 
-    const { data: todos = [], isLoading } = useGetTodosQuery();
+    // const { data: todos = [], isLoading } = useGetTodosQuery();
 
+    const [todoId, setTodoId] = useState(1);
+
+    const { data: todo, isLoading } = useGetTodoQuery(todoId);
+
+
+    const prevTodo = () => {
+        if (todoId === 1) return;
+        setTodoId(todoId - 1)
+    }
+
+    const nextTodo = () => {
+        setTodoId(todoId + 1)
+    }
     return (
         <>
             <h1>Todos - RTK Query</h1>
             <hr />
             <h4>{isLoading ? 'True' : 'False'}</h4>
 
-            <pre></pre>
-            <ul>
+            <pre>{JSON.stringify(todo)}</pre>
+            {/* <ul>
                 {todos.map(({ id, title, completed }) => (
                     <li key={id}>
                         <strong>{completed ? 'DONE' : 'PENDING'}</strong> {title}
                     </li>
                 ))}
-            </ul>
-            <button>
-                next todo
+            </ul> */}
+
+            <button onClick={prevTodo}>
+                prev Todo
             </button>
+
+            <button onClick={nextTodo}>
+                next todo
+            </button >
         </>
     )
 }
